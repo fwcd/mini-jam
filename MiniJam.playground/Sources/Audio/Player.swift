971b5e6fbeeca9b7f7a59454cb74f8f6e05ef69e
fwcd <fwcdmail@gmail.com>
2020-05-18 00:09:57 +0200

    Always perform playback

--- MiniJam.playground/Sources/Audio/Player.swift
+++ MiniJam.playground/Sources/Audio/Player.swift
@@ -12,24 +12,6 @@ public class Player: ObservableObject {
     @Published public var isPlaying: Bool = false {
         willSet {
             if newValue {
-                var playingNotes = Set<Note>()
-                
-                cancellable = timelineTimer.$time.sink {
-                    do {
-                        let currentNotes = Set(self.tracks.notesAt(time: $0))
-                        if playingNotes != currentNotes {
-                            for note in playingNotes {
-                                try self.synthesizer.stop(note: note)
-                            }
-                            for note in currentNotes {
-                                try self.synthesizer.start(note: note)
-                            }
-                            playingNotes = currentNotes
-                        }
-                    } catch {
-                        print("Could not synthesize sound while playing: \(error)")
-                    }
-                }
                 timelineTimer.start()
             } else {
                 timelineTimer.stop()
@@ -41,5 +23,24 @@ public class Player: ObservableObject {
         self.tracks = tracks
         self.timelineTimer = timelineTimer
         self.synthesizer = synthesizer
+        
+        // Subscribe to the timer
+        var playingNotes = Set<Note>()
+        cancellable = timelineTimer.$time.sink {
+            do {
+                let currentNotes = Set(self.tracks.notesAt(time: $0))
+                if playingNotes != currentNotes {
+                    for note in playingNotes {
+                        try self.synthesizer.stop(note: note)
+                    }
+                    for note in currentNotes {
+                        try self.synthesizer.start(note: note)
+                    }
+                    playingNotes = currentNotes
+                }
+            } catch {
+                print("Could not synthesize sound while playing: \(error)")
+            }
+        }
     }
 }
